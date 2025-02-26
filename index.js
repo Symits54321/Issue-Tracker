0109c0537c6977707a52897a19b2ef50cadb9153
Symits54321 <125802160+Symits54321@users.noreply.github.com>
2025-02-26 13:30:22 +0530

    fix cors again 1

--- index.js
+++ index.js
@@ -3,52 +3,40 @@ const dotenv = require('dotenv').config();
 
 const port = 8100;
 
-
-
 // Requiring dependencies
 const express = require('express');
 const cors = require('cors');
-const cookieParser=require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-//const session = require('express-session');
-//const passport = require('passport');
-//const passportLocal = require('./config/passport-local-strategy');
 const path = require('path');
 
 // cors error handler
-const allowedOrigins = [
-    'https://issue-tracker-symits.onrender.com',
-    // Add any other allowed origins here.
-  ];
-  
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
-  
-  app.use(cors(corsOptions));
+const allowedOrigin = 'https://issue-tracker-symits.onrender.com';
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin === allowedOrigin || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 //SASS
-const sassMiddleware =require('node-sass-middleware');
+const sassMiddleware = require('node-sass-middleware');
 
 app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
-    debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
+  src: './assets/scss',
+  dest: './assets/css',
+  debug: true,
+  outputStyle: 'extended',
+  prefix: '/css'
 }));
-
-
-
 
 //Parsers
 const bodyParser = require('body-parser');
@@ -62,83 +50,30 @@ app.use(express.static('assets'));
 
 //layout
 app.use(expressLayouts);
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
-
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // ------- Middlewares
 
 // EJS 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// for mobile viw testing( CORS error handling)
+// for mobile view testing (CORS error handling)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://issue-tracker-symits.onrender.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
-
-// Session
-/*
-app.use(session({
-  name: 'codeial',
-  // TODO change the secret before deployment in production mode
-  secret: 'blahsomething',
-  saveUninitialized: false,
-  resave: false,
-  cookie: {
-      maxAge: (1000 * 60 * 100)
-  },
-  store: new MongoStore(
-      {
-          uri: 'mongodb://127.0.0.1:27017/social_media9956',
-          autoRemove: 'disabled'
-
-      },
-      function (err) {
-          console.log(err || 'connect-mongodb setup ok');
-      }
-  )
-}));
-*/
-  
-  
-  
-  
-
-// passport
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(passport.setAuthenticatedUser);
-
-
-
-//routes
-
-app.use('/',require('./routes/index'));
-
-
-
-
-
-
-
-
-
-// Listening
-app.listen(port,function(err){
-    if(err){
-        console.log('not able to listen port');
-    }
-    console.log(`Issue Tracker is Listening to port:${port}`);
-    
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
+//routes
+app.use('/', require('./routes/index'));
 
-
-
- 
+// Listening
+app.listen(port, function (err) {
+  if (err) {
+    console.log('not able to listen port');
+  }
+  console.log(`Issue Tracker is Listening to port: ${port}`);
+});
